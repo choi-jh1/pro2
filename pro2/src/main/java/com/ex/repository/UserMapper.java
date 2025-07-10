6cce35474bd0a284fbbda5f8c0d559cf4e28d466
choi-jh1 <star6248@naver.com>
2025-07-10 17:35:06 +0900

    ss

--- pro2/src/main/java/com/ex/repository/UserMapper.java
+++ pro2/src/main/java/com/ex/repository/UserMapper.java
@@ -1,7 +1,9 @@
 package com.ex.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @Mapper
 public interface UserMapper {
 	
