c1c7a90425777698b42cd2545149fdb540a66bab
godsu111 <misspotune77@gmail.com>
2025-07-14 17:52:08 +0900

    1
    1

--- pro2/src/main/java/com/ex/service/ReportService.java
+++ pro2/src/main/java/com/ex/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.ex.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ex.data.ReportBoardDTO;
 import com.ex.repository.ReportMapper;
