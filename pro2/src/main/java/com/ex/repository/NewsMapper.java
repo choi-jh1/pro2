2a75b3b6b408f120e143e105a9549a0f6f7ed06f
choi-jh1 <star6248@naver.com>
2025-07-14 18:15:15 +0900

    ㅁㄴㅇ
    ㅁㄴㅇ

--- pro2/src/main/java/com/ex/repository/NewsMapper.java
+++ pro2/src/main/java/com/ex/repository/NewsMapper.java
@@ -1,10 +1,12 @@
 package com.ex.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import com.ex.data.NewsDTO;
 
 @Mapper
+@Repository
 public interface NewsMapper {
 	// 메인 뉴스
 	public NewsDTO mainNews();
