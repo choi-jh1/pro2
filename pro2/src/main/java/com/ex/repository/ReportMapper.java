2bebe99897dd501da027437e309e85231068195b
godsu111 <misspotune77@gmail.com>
2025-07-15 16:14:04 +0900

    123
    123

--- pro2/src/main/java/com/ex/repository/ReportMapper.java
+++ pro2/src/main/java/com/ex/repository/ReportMapper.java
@@ -4,11 +4,12 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import com.ex.data.ReportBoardDTO;
-import com.ex.data.ReportBoardFileDTO;
+
 
 @Mapper
 @Repository
 public interface ReportMapper {
+	// 글쓰기
 	public void insertReport(ReportBoardDTO dto);
-	public void insertReportBoardFile(ReportBoardFileDTO fdto);
+	
 }
--- pro2/src/main/java/com/ex/service/ReportService.java
+++ pro2/src/main/java/com/ex/service/ReportService.java
@@ -1,7 +1,6 @@
 package com.ex.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.ex.data.ReportBoardDTO;
 import com.ex.repository.ReportMapper;
@@ -13,9 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 public class ReportService {
 	private final ReportMapper reportMapper;
-	
-	@Transactional 
 	public void insert(ReportBoardDTO dto) {
-		
+		reportMapper.insertReport(dto);
 	}
 }
