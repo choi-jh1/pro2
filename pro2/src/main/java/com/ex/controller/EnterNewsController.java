c95c35da3fe3655abd7e7600b80e40eddba57d51
Joon12558 <sesus12@naver.com>
2025-07-16 17:10:01 +0900

    연예뉴스 메인페이지
    enter/main.html 생성 완

--- pro2/src/main/java/com/ex/controller/EnterNewsController.java
+++ pro2/src/main/java/com/ex/controller/EnterNewsController.java
@@ -1,7 +1,31 @@
 package com.ex.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ex.data.EnterNewsDTO;
+import com.ex.service.EnterNewsService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/enter")
 public class EnterNewsController {
+	private final EnterNewsService enterNewsService;
+	
+	@GetMapping("/main")
+	public String enterMain(Model model) {
+        List<EnterNewsDTO> top10 = enterNewsService.getTop10DailyNews();
+        List<EnterNewsDTO> latestNews = enterNewsService.getPagedEnterNews(0, 10);
+
+        model.addAttribute("top10", top10);
+        model.addAttribute("latestNews", latestNews);
+		
+		return "enter/main";
+	}
 }
