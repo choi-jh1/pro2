0a6b3b0827e3eb19764d15d2706366e9c546810f
Joon12558 <sesus12@naver.com>
2025-07-16 15:08:45 +0900

    연예뉴스 DTO
    연예기사DTO 수정 및 연예댓글 DTO 생성

--- pro2/src/main/java/com/ex/data/EnterNewsDTO.java
+++ pro2/src/main/java/com/ex/data/EnterNewsDTO.java
@@ -6,14 +6,21 @@ import lombok.Data;
 
 @Data
 public class EnterNewsDTO {
-	private int num;
-	private String title;
-	private String content;
-	private String writer;
-	private LocalDateTime reg;
-	private int readCount;
-	private String category;
-	private int hot;
+    private int num;
+    private String title;
+    private String content;
+
+    // FK: users.id (VARCHAR2)
+    private String writerId;
+
+    // JOIN 결과로 가져올 값
+    private String writerNickname;
+    private String writerEmail;
+
+    private LocalDateTime reg;
+    private int readCount;
+    private String category;
+    private int hot;
+    
+    private int status;
 }
-
-
