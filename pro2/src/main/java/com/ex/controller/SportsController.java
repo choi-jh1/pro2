b5e139af765736d4c8381efd23d9b7e873312723
godsu111 <misspotune77@gmail.com>
2025-07-18 22:06:13 +0900

    1
    1

--- pro2/src/main/java/com/ex/controller/SportsController.java
+++ pro2/src/main/java/com/ex/controller/SportsController.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
