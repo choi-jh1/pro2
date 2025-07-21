22157e2a2efb61932543f0e6439a63e85d2b1f98
Ahn0204 <ahn02041@gmail.com>
2025-07-21 15:07:16 +0900

    충돌 수정

--- pro2/src/main/java/com/ex/service/ReportService.java
+++ pro2/src/main/java/com/ex/service/ReportService.java
@@ -1,7 +1,5 @@
 package com.ex.service;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import com.ex.data.ReportBoardDTO;
